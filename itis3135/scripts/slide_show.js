e7c872e94909756cf86e49859a6be48cd6074a30
ZacharyPrivett <zprivet0@email.cpcc.edu>
2022-03-31 17:47:13 -0400

    added js include

--- itis3135/scripts/slide_show.js
+++ itis3135/scripts/slide_show.js
@@ -1,34 +1,48 @@
 // Code from "Coding Passive Income" YouTube Channel
 // https://www.youtube.com/watch?v=JQSxHen1-yI
+let stopSlideshow = false;
 
 function slideShow(caller){
+    let status = $(caller).attr('value');
+
+    if (status.indexOf('Start') > -1) {
+        stopSlideshow = false;
+        $(caller).attr('value', 'Stop Slideshow');
+    } else {
+        stopSlideshow = true;
+        $(caller).attr('value', 'Start Slideshow');
+    }
+
     let interval = setInterval(function () {
-        changeSlide('next');
-    }, 1000)
+        if (!stopSlideshow)
+            changeSlide('next');
+        else
+            clearInterval(interval)    
+    }, 1000);
 }
 
 
 function changeSlide(direction) {
-    let currentImg = $('active');
+    let currentImg = $('.active');
     let nextImg = currentImg.next();
     let prevImg = currentImg.prev();
 
     if (direction == 'next') {
-        if (nextImg.length) {
+        if (nextImg.length)
             nextImg.addClass('active');
-        } else {
+        else 
             $('.slider img').first().addClass('active');
-        }   
+           
     } else {
         if (prevImg.length) {
             prevImg.addClass('active');
         } else {
             $('.slider img').last().addClass('active')
         }
-            
+        
     }
-
-    currentImg.removeClass('active');
+    currentImg.removeClass('active');  
+    
 }
 
 /*
