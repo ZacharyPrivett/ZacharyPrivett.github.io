1c874ebf0a33dc5d386bcd546d4f2a28ad6301f6
ZacharyPrivett <zprivet0@email.cpcc.edu>
2022-04-25 10:56:36 -0400

    activity 14

---
+++ itis3135/Activity14/Activity14.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$("#nav_list li").click(function() {
+        $.ajax({
+            url: "json_files/" + $(this).children("a").attr("title") + ".json",
+            dataType: "json",
+            success: function(data) {
+                $.each(data, function() {
+                    $.each(this, function(key, value) {
+                        $("main h1").html(value.title);
+                        $("main h2").html(value.month + "" + value.speaker);
+                        $("main img").attr("src",value.image);
+                        $("main p").html(value.text);
+                    });
+                });
+            }
+
+        });
+    });
+}); // end ready
